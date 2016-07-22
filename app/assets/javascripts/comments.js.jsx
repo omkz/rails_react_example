a1ccd2a64c699802197d4e34cf0080954ac3753c
omz <omenkzz@gmail.com>
2016-07-22 19:13:13 +0700

    hello world from react!

---
+++ app/assets/javascripts/comments.js.jsx
@@ -0,0 +1,19 @@
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <div className='HelloWorld'>
+        Hello, world!
+      </div>
+      );
+  }
+});
+
+var ready = function () {
+  ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('comments')
+  );
+};
+
+$(document).ready(ready);
